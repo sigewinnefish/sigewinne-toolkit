86361a37df13ba9bf96c5789057f7d704122e4cc
Administrator <Administrator>
2025-12-20 03:15:09 +0800

    default window pos and size

--- App6/MainWindow.xaml.h
+++ App6/MainWindow.xaml.h
@@ -27,12 +27,13 @@ namespace winrt::App6::implementation
             this->AppWindow().TitleBar().PreferredHeightOption(TitleBarHeightOption::Tall);
             this->AppWindow().TitleBar().PreferredTheme(TitleBarTheme::UseDefaultAppMode);
             this->AppWindow().SetIcon(L"Assets\\Logo.ico");
-
 			UINT dpi = GetDpiForWindow(GetWindowHandle());
             THROW_HR_IF(E_INVALIDARG, 0 == dpi);
             auto scale = floorf(dpi * 100.0f / 96.0f + 0.5f) / 100.0f;
-            this->AppWindow().Presenter().try_as<OverlappedPresenter>().PreferredMinimumHeight(static_cast<int32_t>(600 * scale));
             this->AppWindow().Presenter().try_as<OverlappedPresenter>().PreferredMinimumWidth(static_cast<int32_t>(1000 * scale));
+            this->AppWindow().Presenter().try_as<OverlappedPresenter>().PreferredMinimumHeight(static_cast<int32_t>(600 * scale));
+        	this->AppWindow().Resize({ static_cast<int32_t>(1600 * scale), static_cast<int32_t>(1000 * scale) });
+            this->AppWindow().Move({ 700, 450 });
 
             // Xaml objects should not call InitializeComponent during construction.
             // See https://github.com/microsoft/cppwinrt/tree/master/nuget#initializecomponent
